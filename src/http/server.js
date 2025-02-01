6a4d08395210bcdc5b3607da2f2ff67f566e032a
Kel <144748800+Kelabr@users.noreply.github.com>
2025-01-31 22:07:46 -0300

    Update server.js
    adicionando 'host' no server http para funcionar no render

--- src/http/server.js
+++ src/http/server.js
@@ -8,7 +8,7 @@ app.register(useRouters, {prefix: 'api'})
 
 
 
-app.listen({port:3333}).then(() => {
+app.listen({port:3333,  host: "0.0.0.0"}).then(() => {
     console.log('http server runing')
 })
 
